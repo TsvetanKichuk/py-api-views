f3e2b04771802b4a58024223266cae05e17bee1d
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-09-29 13:51:58 +0200

    edit models and migrations

--- cinema/models.py
+++ cinema/models.py
@@ -4,16 +4,16 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.first_name, self.last_name
+    def __str__(self) -> str:
+        return f"{self.first_name}, {self.last_name}"
 
 
 class Movie(models.Model):
@@ -23,8 +23,8 @@ class Movie(models.Model):
     genres = models.ManyToManyField(Genre, related_name="movies")
     actors = models.ManyToManyField(Actor, related_name="movies")
 
-    def __str__(self):
-        return self.title, self.description, self.duration, self.genres
+    def __str__(self) -> str:
+        return f"{self.title}, {self.description}, {self.duration}, {self.genres}"
 
 
 class CinemaHall(models.Model):
@@ -32,5 +32,5 @@ class CinemaHall(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
-    def __str__(self):
-        return self.name, self.rows, self.seats_in_row
+    def __str__(self) -> str:
+        return f"{self.name}, {self.rows}, {self.seats_in_row}"
